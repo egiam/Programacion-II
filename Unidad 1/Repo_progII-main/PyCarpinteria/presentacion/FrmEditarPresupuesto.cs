c79f3be06ab8e9fd90eb6cdb80d436b38dacbce3
Ezequiel <ezegiampaoli@gmail.com>
2021-09-29 09:33:32 -0300

    .

--- Unidad 1/Repo_progII-main/PyCarpinteria/presentacion/FrmEditarPresupuesto.cs
+++ Unidad 1/Repo_progII-main/PyCarpinteria/presentacion/FrmEditarPresupuesto.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PyCarpinteria.servicios;
 
 namespace PyCarpinteria.presentacion
 {
     public partial class FrmEditarPresupuesto : Form
     {
+        private IService servicio;
+        private Accion modo;
+
         public FrmEditarPresupuesto()
         {
             InitializeComponent();
+        }
+
+        private void FrmEditarPresupuesto_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
